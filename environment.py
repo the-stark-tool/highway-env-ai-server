240496f88cf69bddd77b3b05af94b6bc43294cd0
Sebastian Betancourt <j.s.betancourt.arias@tue.nl>
2026-05-08 16:27:53 +0200

    fix: x values over 200 in observation

--- environment.py
+++ environment.py
@@ -1,12 +1,14 @@
 import gymnasium as gym
+from highway_env.road.lane import AbstractLane
+from highway_env.vehicle.kinematics import Vehicle
 
-VEHICLE_COUNT = 5
+LANES = 3
 
 ACTIONS_ALL = {0: 'LANE_LEFT', 1: 'IDLE', 2: 'LANE_RIGHT', 3: 'FASTER', 4: 'SLOWER'}
 
 
 ENV_CONFIG = {
-    "lanes_count": 3, 
+    "lanes_count": LANES, 
     "collision_reward": -1,
     "right_lane_reward": 0.1,
     "reward_speed_range": [0,40],
@@ -14,6 +16,17 @@ ENV_CONFIG = {
     "action": {
         "type": "DiscreteMetaAction",
         "target_speeds": [0,5,10,15,20,25,30,35,40]},
+    "observation": {
+        "type": "Kinematics",
+        # Defaults highway_env/envs/common/observation.py:191
+        # overriden with new range for x that allows values over 200
+        "features_range" : { 
+            "x": [0, 1000],
+            "y": [-AbstractLane.DEFAULT_WIDTH * LANES, AbstractLane.DEFAULT_WIDTH * LANES], 
+            "vx": [-2*Vehicle.MAX_SPEED, 2*Vehicle.MAX_SPEED],
+            "vy": [-2*Vehicle.MAX_SPEED, 2*Vehicle.MAX_SPEED]
+        }
+    }
 }
 
 BASE_PATH = ""
